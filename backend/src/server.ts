a2ba7938a1793b919cbb6fc0c18b3af91e0f22ef
Sofia Goszko <sofiagoszko@gmail.com>
2025-07-07 14:04:35 -0300

    Configuración del servidor Express y definición del puerto 8080 por defecto

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`API corriendo en puerto ${PORT}`)
+});
