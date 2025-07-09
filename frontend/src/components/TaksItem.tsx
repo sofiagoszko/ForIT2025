a14dbb948ff050851ac04fbc87fef689f6b36b60
Sofia Goszko <sofiagoszko@gmail.com>
2025-07-08 21:06:11 -0300

    creación del frontend y del componete que muestra los items

---
+++ frontend/src/components/TaksItem.tsx
@@ -0,0 +1,18 @@
+interface Task{
+    id: string,
+    title: string, 
+    description: string,
+    complete: boolean,
+    createdAt: Date
+}
+
+export default function TaskItem({ task }: { task: Task }) {
+  return (
+    <div>
+      <h3>{task.title}</h3>
+      <p><strong>Descripción:</strong> {task.description}</p>
+      <p><strong>Completada:</strong> {task.complete}</p>
+      <p><strong>Fecha de creación:</strong> {new Date(task.createdAt).toLocaleString()}</p>
+    </div>
+  );
+}
